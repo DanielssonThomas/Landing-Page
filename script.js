6d7eb8ffe6dffa53e2343680fb6bd9ea3acd19df
Thomas Danielsson <69715206+DanielssonThomas@users.noreply.github.com>
2022-11-15 15:00:36 +0100

    reverted changes

--- script.js
+++
@@ -1,8 +0,0 @@
-const filterBtn = document.getElementById('filterBtn');
-const filters = document.querySelector('.filter-section');
-const filter = document.querySelector('.filter');
-
-filterBtn.addEventListener('click', () =>{
-    filters.classList.toggle('filter-section');
-    filter.classList.toggle('filter-open');
-});
